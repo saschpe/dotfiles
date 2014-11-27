7025beee344e9885a3fd9b69903c89aed111b5ac
Sascha Peilicke <saschpe@mailbox.org>
2014-11-28 00:04:56 +0100

    Add steam-fix-opengl.sh

---
+++ bin/steam-fix-opengl.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+rm ~/.steam/ubuntu12_32/steam-runtime/i386/usr/lib/i386-linux-gnu/libstdc++.so.6
+rm ~/.steam/ubuntu12_32/steam-runtime/i386/lib/i386-linux-gnu/libgcc_s.so.1
+rm ~/.steam/ubuntu12_32/steam-runtime/amd64/lib/x86_64-linux-gnu/libgcc_s.so.1
+rm ~/.steam/ubuntu12_32/steam-runtime/amd64/usr/lib/x86_64-linux-gnu/libstdc++.so.6
+rm ~/.steam/ubuntu12_32/steam-runtime/i386/usr/lib/i386-linux-gnu/libxcb.so.1
